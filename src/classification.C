85742c62d1c572bc795f0e38f25972b6f4bc812d
Ran Bi <biran@mit.edu>
2019-07-15 15:21:23 -0400

    classification: add base selections and options
    base selections: missing hits, conversion veto, 3d impact parameter.
    improve customisability to avoid recompilation.

--- src/classification.C
+++ src/classification.C
@@ -91,6 +91,11 @@ int classify(configurer* conf, std::string const& output,
     auto type = conf->get<std::vector<uint32_t>>("type");
     auto weight = conf->get<std::string>("weight");
 
+    auto nsig_train = conf->get<int32_t>("nsig_train");
+    auto nbkg_train = conf->get<int32_t>("nbkg_train");
+
+    auto tag = conf->get<std::string>("tag");
+
     /* 0: barrel, 1: endcap */
     auto options = conf->get<int32_t>("options");
 
@@ -116,7 +121,7 @@ int classify(configurer* conf, std::string const& output,
     TFile* fbkg = new TFile(background.data(), "read");
     TTree* tbkg = (TTree*)fbkg->Get("e");
 
-    TFile* fout = TFile::Open((id + "_"s + output).data(), "recreate");
+    TFile* fout = TFile::Open(output.data(), "recreate");
 
     TMVA::Factory* factory = new TMVA::Factory(
         "TMVAClassification",
@@ -125,7 +130,7 @@ int classify(configurer* conf, std::string const& output,
         "Transformations=I:"
         "AnalysisType=Classification");
 
-    TMVA::DataLoader* loader = new TMVA::DataLoader("dataset");
+    TMVA::DataLoader* loader = new TMVA::DataLoader((tag + "_"s + id).data());
     for (auto const& var : variables)
         loader->AddVariable(var.data(), 'F');
 
@@ -139,6 +144,7 @@ int classify(configurer* conf, std::string const& output,
 
     /* selections */
     TCut minpt = "elePt > 20";
+    TCut basic = "eleMissHits <= 1 && eleConvVeto && abs(eleIP3D) < 0.03";
 
     TCut barrel = "abs(eleSCEta) < 1.442";
     TCut endcap = "abs(eleSCEta) > 1.556 && abs(eleSCEta) < 2.4";
@@ -148,13 +154,15 @@ int classify(configurer* conf, std::string const& output,
     TCut unmatch = "gen_index == -1";
     TCut nonprompt = "gen_index != -1 && abs(mcMomPID[gen_index]) > 24";
 
-    TCut sig_sel = minpt && match && region;
-    TCut bkg_sel = minpt && (unmatch || nonprompt) && region;
+    TCut sig_sel = minpt && basic && match && region;
+    TCut bkg_sel = minpt && basic && (unmatch || nonprompt) && region;
+
+    auto train_options = "nTrain_Signal="s + std::to_string(nsig_train)
+        + ":nTrain_Background="s + std::to_string(nbkg_train)
+        + ":SplitMode=Random:NormMode=NumEvents:!V"s;
 
     loader->PrepareTrainingAndTestTree(
-        sig_sel, bkg_sel,
-        "nTrain_Signal=100000:nTrain_Background=20000:"
-        "SplitMode=Random:NormMode=NumEvents:!V");
+        sig_sel, bkg_sel, train_options.data());
 
     /* settings, variable limits */
     static std::string const bounds[3] = { "FMin"s, "FMax"s, "NotEnforced"s };
@@ -184,7 +192,7 @@ int classify(configurer* conf, std::string const& output,
     std::vector<double> upper_bounds;
 
     auto method = dynamic_cast<TMVA::MethodCuts*>(
-        factory->GetMethod("dataset", "CutsGA"));
+        factory->GetMethod((tag + "_"s + id).data(), "CutsGA"));
     method->GetCuts(efficiency, lower_bounds, upper_bounds);
 
     conf->set(id + "_lower"s, std::vector<double>(lower_bounds));
@@ -221,6 +229,7 @@ std::pair<float, float> evaluate(configurer* conf, std::string const& id) {
     TTree* tbkg = (TTree*)fbkg->Get("e");
 
     TCut minpt = "elePt > 20";
+    TCut basic = "eleMissHits <= 1 && eleConvVeto && abs(eleIP3D) < 0.03";
 
     TCut barrel = "abs(eleSCEta) < 1.442";
     TCut endcap = "abs(eleSCEta) > 1.556 && abs(eleSCEta) < 2.4";
@@ -230,8 +239,8 @@ std::pair<float, float> evaluate(configurer* conf, std::string const& id) {
     TCut unmatch = "gen_index == -1";
     TCut nonprompt = "gen_index != -1 && abs(mcMomPID[gen_index]) > 24";
 
-    TCut sig_sel = minpt && match && region;
-    TCut bkg_sel = minpt && (unmatch || nonprompt) && region;
+    TCut sig_sel = minpt && basic && match && region;
+    TCut bkg_sel = minpt && basic && (unmatch || nonprompt) && region;
 
     if (weight.empty()) { weight = "1"s; }
     TCut w(weight.data());
@@ -259,38 +268,21 @@ std::pair<float, float> evaluate(configurer* conf, std::string const& id) {
     float sigeff = static_cast<float>(selsig) / totalsig;
     float bkgrej = 1.f - static_cast<float>(selbkg) / totalbkg;
 
+    printf("\n  [ %.4f / %.4f ]\n\n", sigeff, bkgrej);
+
     return std::make_pair(sigeff, bkgrej);
 }
 
-int main(int argc, char* argv[]) {
-    if (argc != 3) {
-        printf("usage: %s [config] [output]\n", argv[0]);
-        return 1;
-    }
+void draw(configurer* conf, std::string const& output) {
+    auto ids = conf->get<std::vector<std::string>>("ids");
+    auto effs = conf->get<std::vector<float>>("effs");
+    auto cols = conf->get<std::vector<int32_t>>("cols");
+    auto tag = conf->get<std::string>("tag");
 
-    auto conf = new configurer(argv[1]);
-
-    auto stage = conf->get<uint64_t>("stage");
-
-    switch (stage) {
-        case 0: goto _stage0;
-        case 1: goto _stage1;
-        case 2: goto _stage2;
-    }
-
-_stage0:
-    outliers(conf);
-
-_stage1:
-    classify(conf, argv[2], "veto", 0.95);
-    classify(conf, argv[2], "loose", 0.9);
-    classify(conf, argv[2], "medium", 0.8);
-    classify(conf, argv[2], "tight", 0.7);
-
-{
     /* extract roc curve from output file */
-    TFile* f = new TFile(("veto_"s + argv[2]).data(), "read");
-    auto roc = (TH1F*)f->Get("dataset/Method_CutsGA/CutsGA/MVA_CutsGA_rejBvsS");
+    TFile* f = new TFile((ids[0] + "_"s + output).data(), "read");
+    auto roc = (TH1F*)f->Get(
+        (tag + "_"s + ids[0] + "/Method_CutsGA/CutsGA/MVA_CutsGA_rejBvsS").data());
 
     /* lambda to format histogram approriately */
     auto roc_formatter = [](TH1* obj, double min, double max) {
@@ -303,12 +295,12 @@ _stage1:
         obj->GetYaxis()->CenterTitle();
     };
 
-    std::string tag = argv[2];
-    auto ext = tag.find(".root");
+    auto label = output;
+    auto ext = label.find(".root");
     if (ext != std::string::npos)
-        tag.erase(std::begin(tag) + ext, std::end(tag));
+        label.erase(std::begin(label) + ext, std::end(label));
 
-    auto c1 = new paper("working-points-"s + tag);
+    auto c1 = new paper("working-points-"s + label);
     apply_default_style(c1,"pp #sqrt{s} = 5.02 TeV"s, 0., 1.);
     c1->format(std::bind(roc_formatter, _1, 0., 1.));
     c1->set(paper::key);
@@ -324,15 +316,17 @@ _stage1:
         marker->Draw("same");
     };
 
-    std::unordered_map<std::string, int64_t> cmap = {
-        { "veto", 1 }, { "loose", 3 }, { "medium", 4 }, { "tight", 5 } };
+    std::unordered_map<std::string, int64_t> cmap;
+
+    for (int64_t i = 0; i < static_cast<int64_t>(ids.size()); ++i)
+        cmap[ids[i]] = cols[i];
 
     /* print selections and evaluate efficiencies */
     auto variables = conf->get<std::vector<std::string>>("variables");
     auto type = conf->get<std::vector<uint32_t>>("type");
     auto count = static_cast<int64_t>(variables.size());
 
-    for (auto const& id : { "veto"s, "loose"s, "medium"s, "tight"s }) {
+    for (auto const& id : ids) {
         auto lower = conf->get<std::vector<double>>(id + "_lower"s);
         auto upper = conf->get<std::vector<double>>(id + "_upper"s);
 
@@ -354,9 +348,43 @@ _stage1:
     c1->draw("pdf");
 }
 
+int main(int argc, char* argv[]) {
+    if (argc != 3) {
+        printf("usage: %s [config] [output]\n", argv[0]);
+        return 1;
+    }
+
+    auto conf = new configurer(argv[1]);
+
+    auto stage = conf->get<uint64_t>("stage");
+    auto ids = conf->get<std::vector<std::string>>("ids");
+    auto effs = conf->get<std::vector<float>>("effs");
+    auto tag = conf->get<std::string>("tag");
+
+    auto base_tag = tag + "_"s + ids[0] + "_"s + argv[2];
+
+    switch (stage) {
+        case 0: goto _stage0;
+        case 1: goto _stage1;
+        case 2: goto _stage2;
+        case 3: goto _stage3;
+    }
+
+_stage0:
+    outliers(conf);
+
+_stage1:
+    for (int64_t i = 0; i < static_cast<int64_t>(ids.size()); ++i) {
+        auto full_tag = tag + "_"s + ids[i] + "_"s + argv[2];
+        classify(conf, full_tag, ids[i], effs[i]);
+    }
+
 _stage2:
-    TMVA::efficiencies("dataset", ("veto_"s + argv[2]).data(), 2, true);
-    TMVA::variables("dataset", ("veto_"s + argv[2]).data(),
+    draw(conf, base_tag);
+
+_stage3:
+    TMVA::efficiencies(tag.data(), base_tag.data(), 2, true);
+    TMVA::variables(tag.data(), base_tag.data(),
                     "InputVariables_Id", "comparison", false, true);
 
     return 0;
